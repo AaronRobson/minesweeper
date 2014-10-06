b029dd0038b33e832de31d6e3661f75a37a0244f
Aaron Robson <ARobsonSE@hotmail.co.uk>
2014-10-06 23:48:34 +0100

    First non-empty version of the "Func" module. Untested but it does pass the compiler's checks.

---
+++ src/Func.hs
@@ -0,0 +1,85 @@
+module Func
+where
+
+import Data.List (genericLength, genericIndex)
+import Data.Maybe (isNothing, isJust)
+
+type Grid = [GridRow]
+type GridRow = [GridCell]
+data GridCell = GridCell { mine :: MineCell
+                         , marking :: Marking
+                         } deriving (Eq, Show)
+
+type MineCell = Bool
+
+data Marking = Flag | QuestionMark | Normal | Revealed deriving (Eq, Show)
+
+data Progress = InProgress | Won | Lost deriving (Eq, Show)
+
+progress :: Grid -> Progress
+progress g
+  | won g = Won
+  | lost g = Lost
+  | otherwise = InProgress
+  where
+    won :: Grid -> Bool 
+    won grid = and $ map cellValid (concat grid)
+      where
+        cellValid :: GridCell -> Bool
+        cellValid gc = (mine gc) /= (Revealed == marking gc)
+    lost :: Grid -> Bool
+    lost grid = or $ map cellInvalid (concat grid)
+      where
+        cellInvalid :: GridCell -> Bool
+        cellInvalid gc = (mine gc) == (Revealed == marking gc)
+
+finished :: Grid -> Bool
+finished g = InProgress /= progress g
+
+type Location = (Integer,Integer)
+
+numberOfMinesAround :: Grid -> Location -> Integer
+numberOfMinesAround grid (x,y) = cellsThatAreMines
+  where
+    locationsAround :: [Location]
+    locationsAround =
+      [ (x-1,y-1)
+      , (x+0,y-1)
+      , (x+1,y-1)
+
+      , (x-1,y+0)
+      -- Skip the given Location.
+      , (x+1,y+0)
+
+      , (x-1,y+1)
+      , (x+0,y+1)
+      , (x+1,y+1)
+      ]
+    possibleCells :: [Maybe GridCell]
+    possibleCells = map (findCell grid) locationsAround
+    cellsThatAreMines :: Integer
+    cellsThatAreMines = genericLength $ filter maybeCellIsMine possibleCells
+
+findCell :: Grid -> Location -> Maybe GridCell
+findCell grid (x,y) = findRow grid >>= findCell
+  where
+    findRow :: Grid -> Maybe GridRow
+    findRow grid
+      | (y <= 0) && (y < genericLength grid) = Just (genericIndex grid y)
+      | otherwise = Nothing
+    findCell :: GridRow -> Maybe GridCell
+    findCell gridRow
+      | (x <= 0) && (x < genericLength gridRow) = Just (genericIndex gridRow x)
+      | otherwise = Nothing
+
+locationIsMine :: Grid -> Location -> Bool
+locationIsMine grid location = maybeCellIsMine cell
+  where
+    cell = findCell grid location
+
+maybeCellIsMine :: Maybe GridCell -> Bool
+maybeCellIsMine (Just cell) = mine cell
+maybeCellIsMine Nothing = False
+
+main :: IO ()
+main = do putStrLn "The core functionality of the program is in this module."
