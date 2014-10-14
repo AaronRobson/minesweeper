c168f2e34157fa0beee606d643f512022fe4573e
Aaron Robson <ARobsonSE@hotmail.co.uk>
2014-10-14 22:46:27 +0100

    Let a seed or RandomGen be passed in.
    Much of the module is now out of the IO monad and a given grid may be
    deterministically be remade given the same random number generator.

--- src/Gen.hs
+++ src/Gen.hs
@@ -33,19 +33,19 @@ chooseIndex xs index
             before = L.genericTake index xs
             after = L.genericDrop (succ index) xs
 
-chooseRandom :: [a] -> IO (a,[a])
-chooseRandom xs = do index <- randomIndex
-                     return $ chooseIndex xs index
+chooseRandom :: (R.RandomGen r) => r -> [a] -> (a,[a],r)
+chooseRandom gen xs = (randomItem,otherItems,newGen)
   where
-    randomIndex :: IO Integer
-    randomIndex = R.randomRIO (0, pred $ L.genericLength xs)
+    (randomIndex,newGen) = R.randomR (0, pred $ L.genericLength xs) gen
+    (randomItem,otherItems) = chooseIndex xs randomIndex
 
-shuffle :: [a] -> IO [a]
-shuffle [] = return []
-shuffle [x] = return [x]
-shuffle xs = do (chosen,rest) <- chooseRandom xs
-                shuffledRest <- shuffle rest
-                return (chosen:shuffledRest)
+shuffle :: (R.RandomGen r) => r -> [a] -> [a]
+shuffle gen [] = []
+shuffle gen [x] = [x]
+shuffle gen xs = chosen:shuffledRest
+  where
+    (chosen,rest,newGen) = chooseRandom gen xs
+    shuffledRest = shuffle newGen rest
 
 unshuffledGeneratedMines :: Settings -> [F.MineCell]
 unshuffledGeneratedMines (Settings (x,y) mineCount) = concat [(L.genericReplicate paddingCount False), (L.genericReplicate mineCount True)]
@@ -55,8 +55,8 @@ unshuffledGeneratedMines (Settings (x,y) mineCount) = concat [(L.genericReplicat
     totalNumberOfCells:: Integer
     totalNumberOfCells = x*y
 
-shuffledGeneratedMines :: Settings -> IO [F.MineCell]
-shuffledGeneratedMines settings = shuffle $ unshuffledGeneratedMines settings
+shuffledGeneratedMines :: (R.RandomGen r) => r -> Settings -> [F.MineCell]
+shuffledGeneratedMines gen settings = shuffle gen $ unshuffledGeneratedMines settings
 
 gridify :: Integral i => i -> [a] -> [[a]]
 gridify width _
@@ -66,11 +66,18 @@ gridify width xs = firstRow:(gridify width rest)
   where
     (firstRow, rest) = L.genericSplitAt width xs
 
+generateMineGridFromRandomGen :: (R.RandomGen r) => r -> Settings -> F.MineGrid
+generateMineGridFromRandomGen gen settings = gridify width minesList
+  where Settings (width,_) _ = settings
+        minesList = shuffledGeneratedMines gen settings
+
+generateMineGridFromSeed :: Int -> Settings -> F.MineGrid
+generateMineGridFromSeed seed settings = generateMineGridFromRandomGen gen settings
+  where gen = R.mkStdGen seed
+
 generateMineGrid :: Settings -> IO F.MineGrid
-generateMineGrid settings = do minesList <- shuffledGeneratedMines settings
-                               return $ gridify width minesList
-  where
-    Settings (width,_) _ = settings
+generateMineGrid settings = do gen <- R.newStdGen
+                               return $ generateMineGridFromRandomGen gen settings
 
 main :: IO ()
 main = do putStrLn "Generator of Minesweeper Grids."
@@ -78,3 +85,8 @@ main = do putStrLn "Generator of Minesweeper Grids."
           putStrLn "Example of a random beginner grid:"
           randomGrid <- generateMineGrid beginnerSettings
           print randomGrid
+          putStrLn ""
+          putStrLn $ "Example of a beginner grid with seed of '" ++ (show seed) ++ "':"
+          print seededGrid
+  where seed = 42
+        seededGrid = generateMineGridFromSeed seed beginnerSettings
