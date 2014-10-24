75e4c534ecdf842028b89e6eeb55374e6b067c13
Aaron Robson <ARobsonSE@hotmail.co.uk>
2014-10-24 18:52:00 +0100

    Choosing and displaying the Settings.

--- src/Main.hs
+++ src/Main.hs
@@ -5,6 +5,27 @@ import qualified Text.Read as R
 
 import qualified Gen as G
 
+validateSettingsMaybe :: String -> Maybe G.Settings
+validateSettingsMaybe s =
+  case s of
+    "1" -> Just G.beginnerSettings
+    "2" -> Just G.intermediateSettings
+    "3" -> Just G.advancedSettings
+    _ -> Nothing
+
+chooseSettingsMaybe :: IO (Maybe G.Settings)
+chooseSettingsMaybe = do
+  putStr "Enter 1, 2 or 3 for the setting type: "
+  value <- getLine
+  return $ validateSettingsMaybe value
+
+chooseSettings :: IO G.Settings
+chooseSettings = do
+  mSettings <- chooseSettingsMaybe
+  return $ case mSettings of
+             Just settings -> settings
+             Nothing -> G.beginnerSettings  
+
 validateSeed :: String -> Maybe Int
 validateSeed s = R.readMaybe s :: Maybe Int
 
@@ -19,6 +40,9 @@ chooseSeed = chooseSeedMaybe >>= G.ensureSeed
 
 main :: IO ()
 main = do putStrLn "Minesweeper --- Terminal Interface."
+          settings <- chooseSettings
+          putStr "The settings in use are: "
+          print settings
           seed <- chooseSeed
           putStr "The seed in use is: "
           print seed
